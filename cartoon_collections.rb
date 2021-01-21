66c644afd2b1303011147615acc6e0d1b135aaaa
Tristan Gibbs <gtristan89@gmail.com>
2021-01-20 18:39:59 -0700

    commit tree 4f647fe934609899ff78272f2788dc8d8ee68516

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,32 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarves)# code an argument here
   # Your code here
+  dwarves.each.with_index do |name, i|
+    puts "#{i + 1}. #{name}"
+  end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(planeteer_calls)# code an argument here
   # Your code here
+  planeteer_calls.collect do |call| 
+    call.capitalize + "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(long_calls)# code an argument here
   # Your code here
+  answer = false
+  long_calls.each do |long|
+    if long.length > 4
+      answer = true
+    end
+  end
+  answer
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(cow_gift)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cow_gift.find do |moo|
+    cheese_types.include?(moo)
+  end
 end
